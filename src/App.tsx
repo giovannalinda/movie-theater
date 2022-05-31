70e7472ba08fe869e146f5a584006fac10d8ab07
Giovanna Linda <eugiovannasouza@gmail.com>
2022-05-31 16:01:51 -0300

    feat: defining theme with Stitches

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
+import { globalCss } from '@stitches/react'
+
+const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+  body: {
+    background: '#000000',
+    color: '#FFFFFF',
+    fontFamily: 'Open Sans',
+  },
+  button: {
+    cursor: 'pointer',
+  },
+  a: {
+    textDecoration: 'none',
+  },
+  ul: {
+    listStyle: 'none',
+  },
+})
+
 function App() {
+  globalStyles()
+
   return <h1>Hello World! :D</h1>
 }
 
---
+++ src/stitches.config.ts
@@ -0,0 +1,23 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled, theme, css, config } = createStitches({
+  theme: {
+    fonts: {
+      family: 'Open Sans',
+    },
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      gray300: '#C8C8C8',
+      blue900: '#170352',
+      purple600: '#4B15C6',
+      purple900: '#1D0437',
+      pink400: '#EA58CB',
+    },
+    fontWeights: {
+      regular: '400',
+      medium: '500',
+      bold: '700',
+    },
+  },
+})
